d5bf812bc88286658b774541358b6f0121c0569f
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-27 16:32:31 -0500

    Primera persona

--- EjercicioPersona/Persona.java
+++ EjercicioPersona/Persona.java
@@ -1,3 +1,11 @@
 public class Persona {
-
+    String nombre;
+    int edad = 15;
+    public class  estudiante extends Persona {
+        String matricula;
+        @Override
+        public String toString() {
+            return "El estudiante de nombre: " + nombre + " cuenta con " + edad + " años de edad y su matricula se encuentra en estado " + matricula;
+        }
+    }
 }
