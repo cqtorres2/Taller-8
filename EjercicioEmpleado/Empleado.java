3aa5e01267a757b28a711eaea4f5d65994ee51b4
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-27 16:47:50 -0500

    Primera empleado

--- EjercicioEmpleado/Empleado.java
+++ EjercicioEmpleado/Empleado.java
@@ -1,3 +1,28 @@
 public class Empleado {
+    String nombre;
+    int salario;
 
+    public Empleado(String nombre, int salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+        String mostrarDetalles = "Nombre: " + nombre
+                + " con un salario fijo de " + salario + " actualmente bajo contrato";
+    }
+
+    public class Gerente {
+        public static String mostrarDetalles;
+        String departamento;
+
+        public Gerente(String departamento) {
+            this.departamento = departamento;
+            String mostrarDetalles = "Nombre: " + nombre
+                    + " con un salario fijo de " + salario + " actualmente bajo contrato en el departamento de "
+                    + departamento + " En el pais de colombia";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Los datos son: " + Gerente.mostrarDetalles;
+    }
 }
--- EjercicioPersona/Persona.java
+++ EjercicioPersona/Persona.java
@@ -1,11 +1,14 @@
 public class Persona {
     String nombre;
     int edad = 15;
-    public class  estudiante extends Persona {
+
+    public class estudiante extends Persona {
         String matricula;
+
         @Override
         public String toString() {
-            return "El estudiante de nombre: " + nombre + " cuenta con " + edad + " años de edad y su matricula se encuentra en estado " + matricula;
+            return "El estudiante de nombre: " + nombre + " cuenta con " + edad
+                    + " años de edad y su matricula se encuentra en estado " + matricula;
         }
     }
 }
