effe105e3539696be17559d487830f36176d79e5
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-27 16:21:37 -0500

    Primera vehiculo

--- EjercicioVehiculo/Vehiculo.java
+++ EjercicioVehiculo/Vehiculo.java
@@ -1,3 +1,15 @@
 public class Vehiculo {
+    String Marca;
+    int velocidadMaxima;
 
+    public class Coche extends Vehiculo {
+        int numerodePuertas;
+
+        @Override
+        public String toString() {
+            return "El Coche registrado posee una marca de nombre; " + Marca + " Con una velocidad maxima de "
+                    + velocidadMaxima + " caballos de fuerza y " + numerodePuertas + " puertas";
+        }
+
+    }
 }
