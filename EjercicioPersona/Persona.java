c90f39f78f9ef625308aa7c719780efc7ce4bfb5
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-29 11:45:03 -0500

    ultimo persona

--- EjercicioPersona/Persona.java
+++ EjercicioPersona/Persona.java
@@ -2,8 +2,25 @@ public class Persona {
     String nombre;
     int edad = 15;
 
+    public void Detalles() {
+        System.out.println("El estudiante de nombre: " + nombre + " cuenta con " + edad
+                + " años de edad");
+    }
+
     public class estudiante extends Persona {
         String matricula;
+
+        public estudiante(String nombre, String matricula, int edad){
+            this.edad = edad;
+            this.nombre = nombre;
+            this.matricula = matricula;
+        }
+
+        @Override
+        public void Detalles() {
+            System.out.println("El estudiante de nombre: " + nombre + " cuenta con " + edad
+                    + " años de edad y su matricula se encuentra en estado " + matricula);
+        }
 
         @Override
         public String toString() {
--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-       Empleado empleado = new Empleado();
-        Empleado.Gerente emp1 = empleado.new Gerente ("Boliva", "rube", 34);
-
+        Empleado empleado = new Empleado();
+        Persona persona = new Persona();
+        Empleado.Gerente emp1 = empleado.new Gerente("Boliva", "rube", 34);
+        Persona.estudiante per1 = persona.new estudiante("Lucas", "Admitido", 15);
         System.out.println(emp1);
+        System.out.println(per1);
     }
 }
