8964d349bd7fbe19323ad72d6ab47783910531d0
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-29 11:51:34 -0500

    ultimo vehicula

--- EjercicioVehiculo/Vehiculo.java
+++ EjercicioVehiculo/Vehiculo.java
@@ -2,8 +2,19 @@ public class Vehiculo {
     String Marca;
     int velocidadMaxima;
 
+    public void Mostrar() {
+        System.out.println("El estudiante de nombre: " + Marca + " cuenta con " + velocidadMaxima
+                + " años de edad");
+    }
+
     public class Coche extends Vehiculo {
         int numerodePuertas;
+
+        public Coche(String marca, int velocidadMaxima, int numerodePuertas) {
+            this.Marca = marca;
+            this.velocidadMaxima = velocidadMaxima;
+            this.numerodePuertas = numerodePuertas;
+        }
 
         @Override
         public String toString() {
--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Empleado empleado = new Empleado();
         Persona persona = new Persona();
+        Vehiculo vechiculo = new Vehiculo();
+
         Empleado.Gerente emp1 = empleado.new Gerente("Boliva", "rube", 34);
         Persona.estudiante per1 = persona.new estudiante("Lucas", "Admitido", 15);
+        Vehiculo.Coche coh1 = vechiculo.new Coche("Chevrolet", 45, 4);
+
         System.out.println(emp1);
         System.out.println(per1);
+        System.out.println(coh1);
     }
 }
